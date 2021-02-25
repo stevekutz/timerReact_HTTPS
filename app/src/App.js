45535996f70c4165724b06ff98d0fbd0e86e8b1d
stevekutz <stkutz@gmail.com>
2021-02-25 14:24:27 -0600

    added basic Timer comp

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Timer from './comp/Timer';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <header className="App-header">
+
+        <Timer />    
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ app/src/comp/Timer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Timer = (props) => {
+
+    const currentDate = new Date().toDateString()
+
+    return (
+        <div>
+            <p> Timer says date is:  { currentDate} </p>
+        
+        </div>
+    
+    
+    )
+
+
+
+}
+
+export default Timer
