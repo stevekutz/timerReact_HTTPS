50f9839e35a0fc565c744b3b568934029eb5dece
stevekutz <stkutz@gmail.com>
2021-02-26 18:28:59 -0600

    added useEffect for button Start/Stop label & added timer reset functionality

--- app/src/comp/Timer.js
+++ app/src/comp/Timer.js
@@ -1,42 +1,75 @@
 import React, {useState, useEffect} from 'react';
 
+
+
 const Timer = (props) => {
 
+    const currentDate = new Date().toDateString();
+    
     const [secondsCount, setSecondsCount] = useState(0);
+    const [timerActive, setTimerActive] = useState(false);
+    const [timerStartStop, setTimerStartStop] = useState('Start');
+
+    const [timerDirection, setTimerDirection] = useState('up');
     // const [ timeCount, setTimeCount] = useState(new Date());
 
-    const countTime = (dir) => {
-        if (dir) {
-            setInterval(() => setSecondsCount(secondsCount + 1), 1000)
-            console.log("secondsCount", secondsCount);
-        }
+    // const countTime = (dir) => {
+    //     if (dir) {
+    //         setTimerDirection('up')
+    //     }
     
+    
+    // }
+
+
+    const toggleTimer = () => {
+        setTimerActive(!timerActive);
+    
+    }
+
+    const resetTimer = () => {
+        toggleTimer();
+        setSecondsCount(0);
+
     
     }
 
 
+    // toggle button control text for Start / Stop
+    useEffect(() => {
+        timerActive ? setTimerStartStop('Stop') : setTimerStartStop('Start')
+    
+    }, [timerActive])
+
+
     // similar to mix of componentDidMount + componentDidUpdate + componentWillUnmount
     useEffect( () => {
-        const timer = setInterval(
-            () => {setSecondsCount(secondsCount + 1)}
-        , 1000)
+        if(timerActive) {
+            const timer = setInterval(
+                () => {setSecondsCount(secondsCount + 1)}
+            , 1000)
 
-        // cancels operation of timer, this allows single increment else this starts count up from 0
-        return () => clearInterval(timer);
+            // cancels operation of timer, this allows single increment else this starts count up from 0
+            return () => clearInterval(timer);
+        
+        
+        }
     
     })
 
 
-    const currentDate = new Date().toDateString()
-
-    
-
 
 
     return (
+
+
         <div>
             <p> Direct call to date is:  { currentDate} </p>
-            <button onClick = {() => countTime(true) }> Start Timer </button>
+            <div>
+                <button onClick = {toggleTimer}> {timerStartStop} </button>
+                <button onClick = {resetTimer}> Reset </button>
+            
+            </div>
             <h2> Seconds Count : {secondsCount}</h2>
 
             
